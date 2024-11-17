0b249e804a51db2a0a980a25f553203985ae3d53
xiryg <xry102410@gmail.com>
2024-11-17 20:19:58 +0800

    improvement: 删除部分代码

--- src/detector_yolo/batch_detect.py
+++ src/detector_yolo/batch_detect.py
@@ -1,17 +1,13 @@
-from ultralytics import YOLO
-import os
-import cv2
 import os
 
-print("Current working directory:", os.getcwd())
+import cv2
+from ultralytics import YOLO
 
 # 加载模型
 model = YOLO("./weights/best.pt")  # 替换为你的模型路径
 
 input_folder = "D:/PycharmProjects/LPRNet-YOLO-IP-License-Plate-Recognition/data/ccpd_data/test/"
 output_folder = "D:/PycharmProjects/LPRNet-YOLO-IP-License-Plate-Recognition/data/ccpd_detect_out/"
-
-os.makedirs(output_folder, exist_ok=True)
 
 image_files = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if
                f.lower().endswith(('.png', '.jpg'))]
@@ -22,7 +18,7 @@ for image_path in image_files:
     for i, result in enumerate(results):  # 遍历每个结果
         # 绘制图像
         annotated_frame = result.plot()
-
+        # 保存
         save_path = os.path.join(output_folder, os.path.basename(image_path))
         cv2.imwrite(save_path, annotated_frame)
         print(f"结果已保存到: {save_path}")
